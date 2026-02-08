b7f90d38ef1cb1dfb760c0f9adcf612df0f0263f
willianleiton <willianleitondev@gmail.com>
2026-02-08 16:55:43 -0500

    Example working

--- main.ts
+++ main.ts
@@ -1,42 +1,43 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from 'zod'
+import { z } from "zod";
 
 const server = new McpServer({
-  name: 'Demo',
-  version: '1.0.0',
-})
+  name: "Demo",
+  version: "1.0.0",
+});
 
 server.registerTool(
-    'fetch-weather',
+  "fetch-weather",
     {
-      title: 'Fetch Weather',
-      description: 'Fetch weather for a certain hour of the current day and only for the city Cali, in Colombia',
+    title: "Fetch Weather",
+    description:
+      "Fetch weather for a certain hour of the current day and only for the city Cali, in Colombia",
       inputSchema: {
-        hour: z.string().describe('Hour to check')
-      }
+      hour: z.string().describe("Hour to check"),
+    },
     },
     async ({ hour }) => {
     return {
       content: [
         {
-          type: 'text',
-          text: `The hour is ${hour}`
-        }
-      ]
+          type: "text",
+          text: `The hour is ${hour}`,
+        },
+      ],
+    };
     }
-  }
-)
+);
 
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
-  console.log('MCP server is running...');
+  // console.log('MCP server is running...');
 }
 
 try {
   await main();
 } catch (error) {
-  console.error('Server error:', error);
+  // console.error('Server error:', error);
 }
 
